22160be5773d8e18add8edc6b6fe3321359d7820
Gongyixuan0315 <862229310@qq.com>
2016-04-12 18:31:52 +0800

    这破作业

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "862229310@qq.com"
+git config --global user.name "Gongyixuan0315"
+git config --global push.default simple
+git add .
+git commit -$1
+git push
+start https://Gongyixuan0315.github.io
